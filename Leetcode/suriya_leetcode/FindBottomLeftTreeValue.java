436909bae1b993ab422d0900777dc79f49da6553
Suriyaprakash1101 <123788954+Suriyaprakash1101@users.noreply.github.com>
2024-10-18 15:33:19 +0530

    Add files via upload

---
+++ Leetcode/suriya_leetcode/FindBottomLeftTreeValue.java
@@ -0,0 +1,65 @@
+/*
+Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: 1
+Example 2:
+
+
+Input: root = [1,2,3,4,null,5,6,null,null,7]
+Output: 7
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode>q=new LinkedList<>();
+        if(root==null){
+            return 0;
+        }
+        q.offer(root);
+        int lef=0;
+        while(!q.isEmpty()){
+            int len=q.size();
+            for(int i=0;i<len;i++){
+                TreeNode current=q.poll();
+                if(i==0){
+                    lef=current.val;
+                }
+                if(current.left!=null){
+                    q.offer(current.left);
+                }
+                if(current.right!=null){
+                    q.offer(current.right);
+                }
+            }
+        }
+        return lef;
+    }
+}
+
